17307a36a7141d186d15c9c037fbdf89abac51db
Tyler Baker <tyler.baker@linaro.org>
2016-09-02 13:51:19 -0700

    Move lava service to proper namespace

--- deploy-lava-dev.sh
+++ deploy-lava-dev.sh
@@ -1,2 +1,3 @@
+kubectl --namespace=lava delete service lava-dev
 kubectl create -f deployments/lava-dev.yaml --record
-kubectl expose -f deployments/lava-dev.yaml --port=9000 --target-port=80 --external-ip="192.168.1.2"
+kubectl expose --namespace=lava -f deployments/lava-dev.yaml --port=9000 --target-port=80 --external-ip="192.168.1.2"
--- deploy-lava-kci.sh
+++ deploy-lava-kci.sh
@@ -1,2 +1,3 @@
+kubectl --namespace=lava delete service lava-kci
 kubectl create -f deployments/lava-kci.yaml --record
-kubectl expose -f deployments/lava-kci.yaml --port=9001 --target-port=80 --external-ip="192.168.1.2"
+kubectl expose --namespace=lava -f deployments/lava-kci.yaml --port=9001 --target-port=80 --external-ip="192.168.1.2"
