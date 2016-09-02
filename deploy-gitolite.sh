889479b2f0c7c9304964d323084e324f3ec006c3
Tyler Baker <tyler.baker@linaro.org>
2016-09-02 10:15:06 -0700

    Fix typo in deploy-gitolite

--- deploy-gitolite.sh
+++ deploy-gitolite.sh
@@ -1,6 +1,6 @@
 kubectl delete service gitolite
 kubectl delete service redis
 kubectl create -f deployments/redis.yaml --record
-kubectl expose -f deployemtns/redis.yaml --port=6379 --target-port=6379
+kubectl expose -f deployments/redis.yaml --port=6379 --target-port=6379
 kubectl create -f deployments/gitolite.yaml --record
 kubectl expose -f deployments/gitolite.yaml --port=9002 --target-port=22 --external-ip="192.168.1.2"
