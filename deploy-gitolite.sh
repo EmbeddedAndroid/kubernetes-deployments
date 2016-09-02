8d11e31644b2aa0e7f96c613f68e93b10420aa40
Tyler Baker <tyler.baker@linaro.org>
2016-09-02 13:06:36 -0700

    Add simple-storage to gitolite deployment

--- deploy-gitolite.sh
+++ deploy-gitolite.sh
@@ -1,5 +1,8 @@
 kubectl delete service gitolite
 kubectl delete service redis
+kubectl delete service simple-storage
+kubectl create -f deployments/simple-storage.yaml --record
+kubectl expose -f deployments/simple-storage.yaml --port=5000 --target-port=80 --external-ip="192.168.1.2"
 kubectl create -f deployments/redis.yaml --record
 kubectl expose -f deployments/redis.yaml --port=6379 --target-port=6379
 kubectl create -f deployments/gitolite.yaml --record
