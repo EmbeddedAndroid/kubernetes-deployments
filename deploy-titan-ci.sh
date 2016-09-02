1339ae6ba25b298d2699bde17eaf6128a8a52c8e
Tyler Baker <tyler.baker@linaro.org>
2016-09-02 13:13:36 -0700

    Remap simple storage port

--- deploy-titan-ci.sh
+++ deploy-titan-ci.sh
@@ -2,7 +2,7 @@ kubectl delete service gitolite
 kubectl delete service redis
 kubectl delete service simple-storage
 kubectl create -f deployments/simple-storage.yaml --record
-kubectl expose -f deployments/simple-storage.yaml --port=5000 --target-port=80 --external-ip="192.168.1.2"
+kubectl expose -f deployments/simple-storage.yaml --port=5000 --target-port=5000 --external-ip="192.168.1.2"
 kubectl create -f deployments/redis.yaml --record
 kubectl expose -f deployments/redis.yaml --port=6379 --target-port=6379
 kubectl create -f deployments/gitolite.yaml --record
