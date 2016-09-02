dd2c348f3b51ac2c92248d9f3197f12b5511e5f6
Tyler Baker <tyler.baker@linaro.org>
2016-09-02 13:41:35 -0700

    Fix up deployment script to pass the namespace

--- deploy-titan-ci.sh
+++ deploy-titan-ci.sh
@@ -2,8 +2,8 @@ kubectl delete service gitolite
 kubectl delete service redis
 kubectl delete service simple-storage
 kubectl create -f deployments/simple-storage.yaml --record
-kubectl expose -f deployments/simple-storage.yaml --port=5000 --target-port=5000 --external-ip="192.168.1.2"
+kubectl expose --namespace=titan -f deployments/simple-storage.yaml --port=5000 --target-port=5000 --external-ip="192.168.1.2"
 kubectl create -f deployments/redis.yaml --record
-kubectl expose -f deployments/redis.yaml --port=6379 --target-port=6379
+kubectl expose --namespace=titan -f deployments/redis.yaml --port=6379 --target-port=6379
 kubectl create -f deployments/gitolite.yaml --record
-kubectl expose -f deployments/gitolite.yaml --port=9002 --target-port=22 --external-ip="192.168.1.2"
+kubectl expose --namespace=titan -f deployments/gitolite.yaml --port=9002 --target-port=22 --external-ip="192.168.1.2"
