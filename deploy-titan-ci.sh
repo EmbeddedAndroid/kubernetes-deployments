85ee016da6a35428170110c16dc7bcf424379ccb
Tyler Baker <tyler.baker@linaro.org>
2016-09-16 10:26:12 -0700

    nfs => storage

--- deploy-titan-ci.sh
+++ deploy-titan-ci.sh
@@ -1,5 +1,5 @@
-kubectl --namespace=titan delete pvc nfs
-kubectl delete pv nfs
+kubectl --namespace=titan delete pvc storage
+kubectl delete pv storage
 kubectl --namespace=titan delete service gitolite
 kubectl --namespace=titan delete service redis
 kubectl --namespace=titan delete service simple-storage
@@ -7,8 +7,8 @@ kubectl --namespace=titan delete service lava-titan
 kubectl --namespace=titan delete service mariadb
 kubectl --namespace=titan delete service hawkbit
 kubectl --namespace=titan delete service broker
-kubectl create -f volumes/nfs-pv.yaml --record
-kubectl create -f volumes/nfs-pvc.yaml --record
+kubectl create -f volumes/storage-pv.yaml --record
+kubectl create -f volumes/storage-pvc.yaml --record
 kubectl create -f deployments/hawkbit.yaml --record
 kubectl expose --namespace=titan -f deployments/hawkbit.yaml --external-ip="192.168.1.2"
 kubectl create -f deployments/broker.yaml --record
