f59d53855f66486034c988870d2ac09998108454
Tyler Baker <tyler.baker@linaro.org>
2016-09-20 09:56:40 -0700

    Swap order of mariadb and hawkbit

--- deploy-titan-ci.sh
+++ deploy-titan-ci.sh
@@ -13,17 +13,17 @@ kubectl create -f volumes/gitolite-pv.yaml --record
 kubectl create -f volumes/gitolite-pvc.yaml --record
 kubectl create -f volumes/storage-pv.yaml --record
 kubectl create -f volumes/storage-pvc.yaml --record
-kubectl create -f deployments/hawkbit.yaml --record
-kubectl expose --namespace=titan -f deployments/hawkbit.yaml --external-ip="192.168.1.2"
-kubectl create -f deployments/broker.yaml --record
-kubectl expose --namespace=titan -f deployments/broker.yaml
 kubectl create -f deployments/mariadb.yaml --record
 kubectl expose --namespace=titan -f deployments/mariadb.yaml
+kubectl create -f deployments/broker.yaml --record
+kubectl expose --namespace=titan -f deployments/broker.yaml
 kubectl create -f deployments/simple-storage.yaml --record
 kubectl expose --namespace=titan -f deployments/simple-storage.yaml --external-ip="192.168.1.2"
 kubectl create -f deployments/lava-titan.yaml --record
 kubectl expose --namespace=titan -f deployments/lava-titan.yaml --external-ip="192.168.1.2"
 kubectl create -f deployments/redis.yaml --record
 kubectl expose --namespace=titan -f deployments/redis.yaml
+kubectl create -f deployments/hawkbit.yaml --record
+kubectl expose --namespace=titan -f deployments/hawkbit.yaml --external-ip="192.168.1.2"
 kubectl create -f deployments/gitolite.yaml --record
 kubectl expose --namespace=titan -f deployments/gitolite.yaml --port=9002 --target-port=22 --external-ip="192.168.1.2"
