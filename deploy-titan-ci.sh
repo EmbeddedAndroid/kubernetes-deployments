969cf77f4808a3a247d4d6fda3f5b672c92d16d3
Tyler Baker <tyler.baker@linaro.org>
2016-09-23 13:33:19 -0700

    nginx: fix persistentclaimName

--- deploy-titan-ci.sh
+++ deploy-titan-ci.sh
@@ -32,6 +32,6 @@ kubectl create -f deployments/nginx.yaml --record
 kubectl expose --namespace=titan -f deployments/nginx.yaml --external-ip="192.168.1.2"
 kubectl create -f deployments/gitolite.yaml --record
 kubectl expose --namespace=titan -f deployments/gitolite.yaml --port=9002 --target-port=22 --external-ip="192.168.1.2"
-sleep 30
+sleep 60
 kubectl create -f deployments/hawkbit.yaml --record
-kubectl expose --namespace=titan -f deployments/hawkbit.yaml
+kubectl expose --namespace=titan -f deployments/hawkbit.yaml --external-ip="192.168.1.2"
